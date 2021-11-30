a555269d89712110cb0d51b9ec8eb466e4f10c3c
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-11-30 14:27:26 -0600

    This commit gives a rough outline of the schemas.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,30 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    name: String!
+    password: String!
+    hosted_events:[Hosted Events]
+  }
+
+  type  Hosted Events{
+    _id: ID!
+    name: String!
+    event_date: String!
+    event_time:string!
+    event_creation: Date!
+    description: String
+    participants:[Joined Events]
+  }
+
+
+  type Query {
+  }
+
+  type Mutation {
+
+  }
+`;
+
+module.exports = typeDefs;
