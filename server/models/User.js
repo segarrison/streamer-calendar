413f61be2946612514e7cf26629ad34e493e0a29
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-01 12:59:30 -0600

    updated models

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
   username: {
@@ -25,6 +26,18 @@ const UserSchema = new Schema({
     default: Date.now,
   },
 });
+
+UserSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+UserSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("User", UserSchema);
 
