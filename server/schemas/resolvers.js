d20501709903657f5987c4c35bf7d70124b4a701
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-01 12:07:37 -0600

    saving for merge/working on resolvers for login/signup

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
-const { Tech, Matchup } = require('../models');
+const { User, Event } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const {signToken} = require('../utils/auth');
 
 const resolvers = {
   Query: {
   },
   Mutation: {
+      newUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+
+          return {user, token}
+      },
+      login: async (parent, {username, email, password}) =>{
+          const user = await User.findOne({ email });
+          if ()
+      }
   }
 }
 
