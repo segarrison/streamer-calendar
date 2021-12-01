870c070f90399597fa724a42cc58081036f8da90
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-01 11:27:52 -0600

    added auth util and uncommented apollo stuff on server.js

---
+++ server/utils/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+
+
+const secret = 'keep it secret keep it safe';
+const expiration = '2h';
+
+module.exports = {
+
+  authMiddleware: function (req, res, next) {
+
+    let token = req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return res.status(400).json({ message: 'Insert token!' });
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+      return res.status(400).json({ message: 'Token does not work for this game!' });
+    }
+
+    next();
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
