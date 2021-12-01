2cb0da48e85ad5a57a2106e01e8289cf6e164bf8
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-01 14:24:42 -0600

    client side: auth.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
