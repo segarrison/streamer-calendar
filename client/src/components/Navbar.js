efcddd50b534a85cb130852ae9d57cdfd71b2235
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-01 21:48:12 -0600

    front end won't talk to backend

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,20 +15,20 @@ const AppNavbar = () => {
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container fluid>
           <Navbar.Brand as={Link} to='/'>
-            Google Books Search
+            Stream.line
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar'>
             <Nav className='ml-auto'>
-              <Nav.Link as={Link} to='/'>
-                Search For Books
-              </Nav.Link>
-              {/* if user is logged in show saved books and logout */}
+              {/* <Nav.Link as={Link} to='/'>
+                
+              </Nav.Link> */}
+              {/* if user is logged in show events and logout */}
               {Auth.loggedIn() ? (
                 <>
-                  <Nav.Link as={Link} to='/saved'>
-                    See Your Books
-                  </Nav.Link>
+                  {/* <Nav.Link as={Link} to='/somewhere else'> */}
+                    {/* something cool!  */}
+                  {/* </Nav.Link> */}
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
               ) : (
--- server/server.js
+++ server/server.js
@@ -31,6 +31,14 @@ async function startApolloServer(typeDefs, resolvers){
     console.log(error);
   }
 
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
   app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
