85dffea577006ec5a7a76095c4496fe28c67f7f7
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-04 22:03:38 -0600

    can add event, but can't query event by host. events don't appear in user, but user appears in events

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,8 @@ const resolvers = {
   Query: {
     //find all users
     users: async () => {
-      return await User.find({});
+      return await User.find({}).populate("hosted_events").populate("part_events").populate({ path: "part_events", populate: "host" })
+      .populate({ path: "part_events", populate: "part_events" });
     },
 
     //find hostedEvents (by user)
@@ -18,14 +19,23 @@ const resolvers = {
     //find particpantEvents (by user)
     participantEvents: async (parent, { userId }) => {
       return User.findOne({ _id: userId })
-        .populate("part_events")
+        .populate("part_events").populate({ path: "part_events", populate: "host" })
         .populate({ path: "part_events", populate: "part_events" });
     },
 
     // finds eveents and what is connected to them
-    // events : async() => {
-    //   return await Event.find({}).populate('users');
-    // },
+    events : async() => {
+      return await Event.find({}).populate("host").populate("participants");
+    },
+
+    hostedByEvent: async (parent, {userId}) => {
+      data = await Event.find({}).populate("host")
+      user = await User.find({_id: userId})
+      console.log(user);
+      console.log(data);
+      return await Event.find({host: { _id: userId }}).populate("participants");
+      // return await Event.find({host: username }).populate("participants");
+    }
 
     // user : async (parent, { username }) => {
     //   return await User.findOne({ username }).populate('events');
@@ -56,7 +66,14 @@ const resolvers = {
       const token = signToken(user);
       return { user, token };
     },
-    //addEvent
+    addEvent: async (parent, args) =>{
+      console.log(args);
+      userId = args.id;
+      const event = await Event.create(args).then(({_id}) => User.findOneAndUpdate({ where: { _id: userId}}, { $push: { hosted_events: _id }}, {new:true}));
+      
+      
+      return { event };
+    }
     //deleteEvent
     //removeUser (delete user as participant of event)
   },
