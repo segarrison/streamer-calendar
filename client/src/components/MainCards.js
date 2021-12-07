0a8e697762637f6182ec2dd89970776110d4cf4c
Omar D. Ramirez <omardramirez2002@gmail.com>
2021-12-06 22:06:02 -0600

    Tiny styling to cards.

--- client/src/components/MainCards.js
+++ client/src/components/MainCards.js
@@ -7,16 +7,16 @@ const MainCards =()=>{
 
 return(
         <div style = {{display:'flex', justifyContent:'space-around'}}>
-            <Card style = {{width: '25rem'}}> 
-                <Card.Header>Featured</Card.Header>
+            <Card style = {{width: '30rem'}}> 
+                <Card.Header style={{display:'flex',justifyContent:'center'}}>Hosted Events</Card.Header>
                 <ListGroup variant="flush">
                     <ListGroup.Item>Cras justo odio</ListGroup.Item>
                     <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
                     <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
                 </ListGroup>
             </Card>
-            <Card style = {{width: '25rem'}}> 
-                <Card.Header>Featured</Card.Header>
+            <Card style = {{width: '30rem'}}> 
+                <Card.Header style={{display:'flex',justifyContent:'center'}}>Joined Events</Card.Header>
                 <ListGroup variant="flush">
                     <ListGroup.Item>Cras justo odio</ListGroup.Item>
                     <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
