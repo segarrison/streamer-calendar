3c7ea58a2d89575a75feb9e79359152bf9355eb4
segarrison <79775636+segarrison@users.noreply.github.com>
2021-12-07 09:18:27 -0600

    text changes on landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Landing = () => {
+  const styleFont = {
+    fontFamily: "Audiowide"
+  }
+
   return (
     <main>
-      <div className="flex-row justify-center">
-        <h1>Maybe try loggin in?</h1>
+      <div className="flex-row justify-content-center" >
+        <h1 className ="text-center" style= {styleFont}>Stream.line</h1>
+        <h5 className ="text-center">Log in or Sign up to start!</h5>
       </div>
     </main>
   );
